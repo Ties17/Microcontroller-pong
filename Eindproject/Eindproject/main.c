32f3485f7e28a4041084f970121d36ca86791658
Ties <tiestienhoven@gmail.com>
2020-03-04 13:48:53 +0100

    Project made

---
+++ Eindproject/Eindproject/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindproject.c
+ *
+ * Created: 4-3-2020 13:48:24
+ * Author : Ties
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
